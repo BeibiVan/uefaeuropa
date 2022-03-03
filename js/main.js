2f8d7528f5f62edc29be4a53f04f3b515bc6a262
Ivan Dziuba <ivaan.id@gmail.com>
2022-03-03 18:45:45 -0300

    primera parte básica

---
+++ js/main.js
@@ -0,0 +1,128 @@
+const equipoOctavos = document.getElementById ("octavos_container");
+const equipoCuartos = document.getElementById ("cuartos_container");
+
+//SELECCIÓN DE EQUIPO (cuartos de final) - team selection (quarterfinals)
+
+const boton1 = document.querySelector ("#equipo1")
+const boton2 = document.querySelector ("#equipo2")
+const primeroCuartos = document.querySelector ("#equipo17")
+
+
+
+    boton1.addEventListener ("click", ()=> {
+    let primeroCuartos_parrafo = document.createElement("p");
+    let primeroCuartos_texto = document.createTextNode(boton1.innerHTML);
+    primeroCuartos.appendChild(primeroCuartos_texto);
+    })
+    boton2.addEventListener ("click", ()=> {
+    let primeroCuartos_parrafo = document.createElement("p");
+    let primeroCuartos_texto = document.createTextNode(boton2.innerHTML);
+    primeroCuartos.appendChild(primeroCuartos_texto);
+    })
+
+const boton3 = document.querySelector ("#equipo3")
+const boton4 = document.querySelector ("#equipo4")
+const segundoCuartos = document.querySelector ("#equipo18")
+
+    boton3.addEventListener ("click", ()=> {
+    let segundoCuartos_parrafo = document.createElement("p");
+    let segundoCuartos_texto = document.createTextNode(boton3.innerHTML);
+    segundoCuartos.appendChild(segundoCuartos_texto);
+    })
+    boton4.addEventListener ("click", ()=> {
+    let segundoCuartos_parrafo = document.createElement("p");
+    let segundoCuartos_texto = document.createTextNode(boton4.innerHTML);
+    segundoCuartos.appendChild(segundoCuartos_texto);
+    })
+
+const boton5 = document.querySelector ("#equipo5")
+const boton6 = document.querySelector ("#equipo6")
+const terceroCuartos = document.querySelector ("#equipo19")
+
+    boton5.addEventListener ("click", ()=> {
+    let terceroCuartos_parrafo = document.createElement("p");
+    let terceroCuartos_texto = document.createTextNode(boton5.innerHTML);
+    terceroCuartos.appendChild(terceroCuartos_texto);
+    })
+    boton6.addEventListener ("click", ()=> {
+    let terceroCuartos_parrafo = document.createElement("p");
+    let terceroCuartos_texto = document.createTextNode(boton6.innerHTML);
+    terceroCuartos.appendChild(terceroCuartos_texto);
+    })
+
+const boton7 = document.querySelector ("#equipo7")
+const boton8 = document.querySelector ("#equipo8")
+const cuartoCuartos = document.querySelector ("#equipo20")
+
+    boton7.addEventListener ("click", ()=> {
+    let cuartoCuartos_parrafo = document.createElement("p");
+    let cuartoCuartos_texto = document.createTextNode(boton7.innerHTML);
+    cuartoCuartos.appendChild(cuartoCuartos_texto);
+    })
+    boton8.addEventListener ("click", ()=> {
+    let cuartoCuartos_parrafo = document.createElement("p");
+    let cuartoCuartos_texto = document.createTextNode(boton8.innerHTML);
+    cuartoCuartos.appendChild(cuartoCuartos_texto);
+    })
+
+const boton9 = document.querySelector ("#equipo9")
+const boton10 = document.querySelector ("#equipo10")
+const quintoCuartos = document.querySelector ("#equipo21")
+
+    boton9.addEventListener ("click", ()=> {
+    let quintoCuartos_parrafo = document.createElement("p");
+    let quintoCuartos_texto = document.createTextNode(boton9.innerHTML);
+    quintoCuartos.appendChild(quintoCuartos_texto);
+    })
+    boton10.addEventListener ("click", ()=> {
+    let quintoCuartos_parrafo = document.createElement("p");
+    let quintoCuartos_texto = document.createTextNode(boton10.innerHTML);
+    quintoCuartos.appendChild(quintoCuartos_texto);
+    })
+
+const boton11 = document.querySelector ("#equipo11")
+const boton12 = document.querySelector ("#equipo12")
+const sextoCuartos = document.querySelector ("#equipo22")
+
+    boton11.addEventListener ("click", ()=> {
+    let sextoCuartos_parrafo = document.createElement("p");
+    let sextoCuartos_texto = document.createTextNode(boton11.innerHTML);
+    sextoCuartos.appendChild(sextoCuartos_texto);
+    })
+    boton12.addEventListener ("click", ()=> {
+    let sextoCuartos_parrafo = document.createElement("p");
+    let sextoCuartos_texto = document.createTextNode(boton12.innerHTML);
+    sextoCuartos.appendChild(sextoCuartos_texto);
+    })
+
+const boton13 = document.querySelector ("#equipo13")
+const boton14 = document.querySelector ("#equipo14")
+const septimoCuartos = document.querySelector ("#equipo23")
+
+    boton13.addEventListener ("click", ()=> {
+    let septimoCuartos_parrafo = document.createElement("p");
+    let septimoCuartos_texto = document.createTextNode(boton13.innerHTML);
+    septimoCuartos.appendChild(septimoCuartos_texto);
+    })
+    boton14.addEventListener ("click", ()=> {
+    let septimoCuartos_parrafo = document.createElement("p");
+    let septimoCuartos_texto = document.createTextNode(boton14.innerHTML);
+    septimoCuartos.appendChild(septimoCuartos_texto);
+    })
+
+const boton15 = document.querySelector ("#equipo15")
+const boton16 = document.querySelector ("#equipo16")
+const octavoCuartos = document.querySelector ("#equipo24")
+
+    boton15.addEventListener ("click", ()=> {
+    let octavoCuartos_parrafo = document.createElement("p");
+    let octavoCuartos_texto = document.createTextNode(boton15.innerHTML);
+    octavoCuartos.appendChild(octavoCuartos_texto);
+    })
+    boton16.addEventListener ("click", ()=> {
+    let octavoCuartos_parrafo = document.createElement("p");
+    let octavoCuartos_texto = document.createTextNode(boton16.innerHTML);
+    octavoCuartos.appendChild(octavoCuartos_texto);
+    })
+
+    //Selección de equipos semifinales - team selection semifinals
---
+++ js/teams.js
@@ -0,0 +1,26 @@
+class equipo {
+    constructor (equipo,pais) {
+    this.equipo = equipo
+    this.pais = pais
+    }
+};
+
+const equiposTotal = []
+
+equiposTotal.push  (new equipo ("Rangers FC", "Escocia"));
+equiposTotal.push  (new equipo ("Estrella Roja", "Serbia"));
+equiposTotal.push  ( new equipo ("SC Braga", "Portugal"));
+equiposTotal.push  ( new equipo ("AS Monaco", "Francia"));
+equiposTotal.push  ( new equipo ("FC Porto", "Portugal"));
+equiposTotal.push  ( new equipo ("Olympique Lyon", "Francia"));
+equiposTotal.push  ( new equipo ("Atalanta", "Italia"));
+equiposTotal.push  ( new equipo ("Bayer Leverkusen", "Alemania"));
+equiposTotal.push  ( new equipo ("Sevilla", "España"));
+equiposTotal.push  ( new equipo ("West Ham", "Inglaterra"));
+equiposTotal.push  ( new equipo ("Barcelona", "España"));
+equiposTotal.push  ( new equipo ("Galatasaray", "Turquía"));
+equiposTotal.push  ( new equipo ("RB Lepizig", "Alemania"));
+equiposTotal.push  ( new equipo ("Spartak Moscú", "Rusia"));
+equiposTotal.push  (new equipo ("Real Betis", "España"));
+equiposTotal.push  ( new equipo ("Frankfurt", "Alemania"));
+
